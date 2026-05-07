d0e196381844654346f636b34ee5a1c85bf41f06
evanyan1203 <evan.yan1203@gmail.com>
2026-05-07 00:54:14 -0400

    erase

--- Linked-List practice/linked_list.py
+++ Linked-List practice/linked_list.py
@@ -10,18 +10,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def front(self):
-        if self.head == None:
-            raise IndexError("List is empty")
-        return self.head.value
-    
-#  pop_front() - remove the front item and return its value
-    def pop_front(self):
-        if self.head is None:
-            raise IndexError("List is empty")
-        value = self.head.value
-        self.head = self.head.next
-        return value
+
     
 
 #  size() - returns the number of data elements in the list
@@ -54,16 +43,16 @@ class LinkedList:
     def push_front(self, value):
         new_node = Node(value, self.head)
         self.head = new_node
-        return
+        
     
 #  push_back(value) - adds an item at the end
     def push_back(self,value):
 
         new_node = Node(value)
               
-        if self.head == None:
+        if self.head is None:
             self.head = new_node
-            
+            return
 
         current = self.head
 
@@ -71,6 +60,14 @@ class LinkedList:
             current = current.next
 
         current.next = new_node
+
+#  pop_front() - remove the front item and return its value
+    def pop_front(self):
+        if self.head is None:
+            raise IndexError("List is empty")
+        value = self.head.value
+        self.head = self.head.next
+        return value
         
 #  pop_back() - removes end item and returns its value
     def pop_back(self):
@@ -97,9 +94,48 @@ class LinkedList:
 
     
 #  front() - get the value of the front item
+
+    def front(self):
+        if self.head == None:
+            raise IndexError("List is empty")
+        return self.head.value
+    
+
 #  back() - get the value of the end item
+    def back(self):
+        if self.head is None:
+            raise IndexError("List is empty")
+        current = self.head
+        while current.next:
+            current = current.next
+        return current.value
+        
 #  insert(index, value) - insert value at index, so the current item at that index is pointed to by the new item at the index
+
 #  erase(index) - removes node at given index
+    def erase(self,index):
+        if index<0 or index>= self.size():
+            raise IndexError("Index out of bound")
+        
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        current = self.head
+
+        for i in range(index-1):
+            current = current.next
+        
+        index_node = current.next
+        current.next = index.node.next
+        index_node.value = None
+
+
+
+        
+        
+
+
 #  value_n_from_end(n) - returns the value of the node at the nth position from the end of the list
 #  reverse() - reverses the list
 #  remove_value(value) - removes the first item in the list with this value 
--- Linked-List practice/main.py
+++ Linked-List practice/main.py
@@ -5,7 +5,10 @@ lst.push_front(10)
 print(lst.size())
 print(lst.pop_front())
 print(lst.empty())
+lst.push_back(20)
+lst.push_back(30)
 
+print(lst.value_at(1))
 
 
 
