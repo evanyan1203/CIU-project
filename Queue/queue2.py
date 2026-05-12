aeaa4249f1bc3ae09664e4e1fd1f2695cdfb6de0
evanyan1203 <evan.yan1203@gmail.com>
2026-05-11 21:39:50 -0400

    making queue by different way

--- Queue/queue2.py
+++ Queue/queue2.py
@@ -1,8 +1,39 @@
 #  Implement using a fixed-sized array:
-
+class Queue:
+    def __init__(self, capacity = 16):
+        self.capacity = capacity
+        self.size_value = 0
+        self.data = [None] * capacity
 # enqueue(value) - adds item at end of available storage
+    def enqueue(self,value):
+        if self.size_value == self.capacity:
+            raise OverflowError("queue is full")
+
+        self.data[self.size_value] = value
+        self.size_value+=1
+        
 # dequeue() - returns value and removes least recently added element
+    def dequeue(self):
+        if self.size_value ==0:
+            raise IndexError("queue is empty")
+        
+        value = self.data[0]
+        
+        for i in range(self.size_value-1):
+            self.data[i] = self.data[i+1]
+        
+        self.size_value -= 1
+        self.data[self.size_value] = None
+
+        return value
+    
+
 # empty()
+    def empty(self):
+        return self.size_value ==0
+
 # full()
+    def full(self):
+        return self.size_value ==self.capacity
 
 
--- Queue/test.py
+++ Queue/test.py
@@ -1,7 +1,26 @@
-from queue1 import Queue
+# from queue1 import Queue as Queue1
+
+# lst = []
+# q = Queue1()
+# print(q.empty())
+# q.enqueue(1)
+# q.enqueue(2)
+# q.enqueue(3)
+# print(q.empty())
+
+# lst.append(q.dequeue())
+# lst.append(q.dequeue())
+# lst.append(q.dequeue())
+
+# print(q.empty())
+
+# print(lst)
+
+
+from queue2 import Queue as Queue2
 
 lst = []
-q = Queue()
+q = Queue2()
 print(q.empty())
 q.enqueue(1)
 q.enqueue(2)
@@ -15,8 +34,3 @@ lst.append(q.dequeue())
 print(q.empty())
 
 print(lst)
-
-
-
-
-
