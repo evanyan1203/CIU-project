ce461e2faa6d886418bbe0d4164a267bf71f6231
evanyan1203 <evan.yan1203@gmail.com>
2026-05-13 20:08:55 -0400

    hash_table now

--- Queue/queue2.py
+++ Queue/queue2.py
@@ -55,3 +55,11 @@ class Queue:
         return self.size_value ==self.capacity
 
 
+
+
+
+#  Cost:
+# a bad implementation using a linked list where you enqueue at the head and dequeue at the tail would be O(n) because you'd need the next to last element, causing a full traversal of each dequeue
+# enqueue: O(1) (amortized, linked list and array [probing])
+# dequeue: O(1) (linked list and array)
+# empty: O(1) (linked list and array)
